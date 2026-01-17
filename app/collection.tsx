ebf8b35dea930d0a11c59fb75494cd7e92eb6745
Subhamay Dey <ncdey1966@gmail.com>
2026-01-17 19:24:40 +0530

    Added collection route page and dependencies of react snipped entension of rnfes ✅

---
+++ app/collection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const collection = () => {
+  return (
+    <View>
+      <Text>collection</Text>
+    </View>
+  )
+}
+
+export default collection
+
+const styles = StyleSheet.create({})
