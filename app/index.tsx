543cadf07c330b156efc745449d16f3be9259f8a
Subhamay Dey <ncdey1966@gmail.com>
2026-01-17 18:54:52 +0530

    Bug solved ✅ Nativewind is working now ✅

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text>Welcome to the Bird Sound Recognition App!</Text>
+      <Text className="text-5xl">Welcome to the Bird Sound Recognition App!</Text>
     </View>
   );
 }
